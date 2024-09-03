8cdc27d5c6e3d6d654f0a43b498546ec3ae1f224
SHAHRIAR AHMED <shahirarspyy@gmail.com>
2024-09-03 22:44:08 +0600

    😁refactor code

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -1,26 +1,26 @@
 import { z } from "zod";
 
 // Zod schemas for sub-documents
-const userNameValidationSchema = z.object({
+const createUserName = z.object({
    firstName: z
       .string()
-      .min(1, "First name is required")
+      .min(3, "First name is required")
       .refine((value) => /^[A-Z]/.test(value), {
          message: "First Name must start with a capital letter",
       }),
    middleName: z.string().optional(),
    lastName: z
       .string()
-      .min(1, "Last name is required")
+      .min(3, "Last name is required")
       .refine((value) => /^[A-Z]/.test(value), {
          message: "First Name must start with a capital letter",
       }),
 });
 
-const guardianValidationSchema = z.object({
+const createGuardian = z.object({
    fatherName: z
       .string()
-      .min(1, "Father's name is required")
+      .min(3, "Father's name is required")
       .refine((value) => /^[A-Z]/.test(value), {
          message: "First Name must start with a capital letter",
       }),
@@ -28,7 +28,7 @@ const guardianValidationSchema = z.object({
    fatherContactNo: z.string().min(10, "Father's contact number is required"),
    motherName: z
       .string()
-      .min(1, "Mother's name is required")
+      .min(3, "Mother's name is required")
       .refine((value) => /^[A-Z]/.test(value), {
          message: "First Name must start with a capital letter",
       }),
@@ -36,34 +36,37 @@ const guardianValidationSchema = z.object({
    motherContactNo: z.string().min(10, "Mother's contact number is required"),
 });
 
-const localGuardianValidationSchema = z.object({
-   name: z.string().min(1, "Local guardian's name is required"),
+const createLocalGuardian = z.object({
+   name: z.string().min(3, "Local guardian's name is required"),
    occupation: z.string().min(1, "Local guardian's occupation is required"),
    contactNo: z.string().min(10, "Local guardian's contact number is required"),
    address: z.string().min(1, "Local guardian's address is required"),
 });
 
 // Main Zod schema for Student
-const studentValidationSchema = z.object({
-   uid: z.string().min(1, "UID is required"),
-   name: userNameValidationSchema,
-   email: z.string().email().min(1, "Email is required"),
-   contactNo: z.string().min(10, "Contact number is required"),
-   emergencyContactNo: z
-      .string()
-      .min(10, "Emergency contact number is required"),
-   gender: z.enum(["male", "female", "other"]),
-   dateOfBirth: z.string().optional(),
-   bloodGroup: z
-      .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
-      .optional(),
-   presentAddress: z.string().min(1, "Present address is required"),
-   permanentAddress: z.string().min(1, "Permanent address is required"),
-   guardian: guardianValidationSchema,
-   localGuardian: localGuardianValidationSchema,
-   profileImage: z.string().optional(),
-   isDeleted: z.boolean().default(false),
+const create = z.object({
+   body: z.object({
+      password: z.string().min(6).optional(),
+      student: z.object({
+         name: createUserName,
+         email: z.string().email().min(1, "Email is required"),
+         contactNo: z.string().min(10, "Contact number is required"),
+         emergencyContactNo: z
+            .string()
+            .min(10, "Emergency contact number is required"),
+         gender: z.enum(["male", "female", "other"]),
+         dateOfBirth: z.string().optional(),
+         bloodGroup: z
+            .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+            .optional(),
+         presentAddress: z.string().min(1, "Present address is required"),
+         permanentAddress: z.string().min(1, "Permanent address is required"),
+         guardian: createGuardian,
+         localGuardian: createLocalGuardian,
+         profileImage: z.string().optional(),
+      }),
+   }),
 });
 
 // Export the schema
-export const StudentValidation = { studentValidationSchema };
+export const StudentValidationSchema = { create };
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { validateRequest } from "../../middlewares/validate.request";
+import { StudentValidationSchema } from "../student/student.validation";
 
 const router = Router();
 
-router.post("/create-student", UserController.createStudent);
+router.post(
+   "/create-student",
+   validateRequest(StudentValidationSchema.create),
+   UserController.createStudent,
+);
 
 export const UserRoute = router;
